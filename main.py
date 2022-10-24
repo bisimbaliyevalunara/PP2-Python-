18814e9aef95705b1bff41625167b3d0494226e3
bisimbaliyevalunara <98978094+bisimbaliyevalunara@users.noreply.github.com>
2022-10-24 17:20:07 +0600

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from classes.library import LibraryManagmentSystem
-
-library = LibraryManagmentSystem()
-# library.db.insert_borrowed_books()
-# library.db.delete_all_tables()
-# library.db.create_tables()
-#library.db.fill_database()
-library.start()
